5d666480495f8964e9b9d5950cbde225ab7ca37c
safaltaraghav29 <165919797+safaltaraghav29@users.noreply.github.com>
2025-01-23 21:51:34 +0530

    Add files via upload

---
+++ Day2/Demo.java
@@ -0,0 +1,17 @@
+class Demo 
+{
+public static void main(String args[])
+{
+MethodDemo md1=new MethodDemo();
+md1.a=10;
+md1.b=20;
+md1.add();
+md1.sub();
+
+MethodDemo md2=new MethodDemo();
+md2.a=100;
+md2.b=50;
+md2.add();
+md2.sub();
+}
+}
